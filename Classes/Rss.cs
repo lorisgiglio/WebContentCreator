a0062ef89435a40f89d7e5c5cfda039e312ed215
lorisgiglio <loris.giglio@gmail.com>
2025-02-17 18:11:38 +0100

    feat: change confifuration

--- Classes/Rss.cs
+++ Classes/Rss.cs
@@ -30,18 +30,8 @@ namespace WebContentCreator.Classes
         {
             List<KeyValuePair<string, string>> urls = new List<KeyValuePair<string, string>>
             {
-            new KeyValuePair<string, string>("Home", "https://www.ansa.it/sito/ansait_rss.xml"),
-            new KeyValuePair<string, string>("Cronaca", "https://www.ansa.it/sito/notizie/cronaca/cronaca_rss.xml"),
-            new KeyValuePair<string, string>("Politica", "https://www.ansa.it/sito/notizie/politica/politica_rss.xml"),
-            new KeyValuePair<string, string>("Mondo", "https://www.ansa.it/sito/notizie/mondo/mondo_rss.xml"),
-            new KeyValuePair<string, string>("Economia", "https://www.ansa.it/sito/notizie/economia/economia_rss.xml"),
-            new KeyValuePair<string, string>("Calcio", "https://www.ansa.it/sito/notizie/sport/calcio/calcio_rss.xml"),
-            new KeyValuePair<string, string>("Sport", "https://www.ansa.it/sito/notizie/sport/sport_rss.xml"),
-            new KeyValuePair<string, string>("Cinema", "https://www.ansa.it/sito/notizie/cultura/cinema/cinema_rss.xml"),
-            new KeyValuePair<string, string>("Cultura", "https://www.ansa.it/sito/notizie/cultura/cultura_rss.xml"),
-            new KeyValuePair<string, string>("Ultime Notizie", "https://www.ansa.it/sito/notizie/topnews/topnews_rss.xml"),
-            new KeyValuePair<string, string>("Foto", "https://www.ansa.it/sito/photogallery/foto_rss.xml"),
-            new KeyValuePair<string, string>("Video", "https://www.ansa.it/sito/videogallery/video_rss.xml")
+            new KeyValuePair<string, string>("Home", ""),
+
         };
 
             var items = new ConcurrentBag<RssItem>();
